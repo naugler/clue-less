0bffc253060be32d633cc014cc453975e14bc75d
Bob <robertjnaugle@gmail.com>
2014-02-17 16:17:41 -0500

    Basic client and server applications
    These are very skin-and-bones implementations of a server and a client.
    Also included are stubby message handlers. The architecture is of course
    up for debate, so this is just something to get us started.

--- clueless-common/src/com/blakjack/clueless/CluelessMessage.java
+++ clueless-common/src/com/blakjack/clueless/CluelessMessage.java
@@ -2,7 +2,15 @@ package com.blakjack.clueless;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Map.Entry;
 
+/**
+ * This is an example abstract message class. If you don't like it, change it!
+ * Implementing Serializable is not mandatory for our connections, but it's
+ * probably a good idea.
+ * 
+ * @author nauglrj1
+ */
 public abstract class CluelessMessage implements Serializable {
     
     private final Map<String, Serializable> fields;
@@ -21,5 +29,15 @@ public abstract class CluelessMessage implements Serializable {
     
     public void setField(String fieldName, Serializable value) {
         fields.put(fieldName, value);
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(this.getClass().getName());
+        builder.append(":\n");
+        for (Entry<String, Serializable> e : fields.entrySet()) {
+            builder.append(e.getKey()).append(":").append(e.getValue()).append("\n");
+        }
+        return builder.toString();
     }
 }
--- clueless-server/src/com/blakjack/clueless/server/CluelessServer.java
+++ clueless-server/src/com/blakjack/clueless/server/CluelessServer.java
@@ -6,17 +6,61 @@
 
 package com.blakjack.clueless.server;
 
+import com.blakjack.clueless.Connection;
+import com.blakjack.clueless.Connection.MessageHandler;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
- *
+ * This Main class starts up a socket acceptor and handles new connections
+ * 
  * @author nauglrj1
  */
 public class CluelessServer {
-
+    
+    private static final int PORT = 10001;
+    
+    private static CluelessServer server;
+    
+    private Thread acceptorThread = null;
+    private final MessageHandler messageHandler = new ServerMessageHandler();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Starting server...");
+        
+        server = new CluelessServer();
+        server.start();
+    }
+    
+    public CluelessServer() {
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            SocketAcceptor socketAcceptor = new SocketAcceptor(address, PORT);
+            socketAcceptor.addSocketAcceptorListener(new SocketAcceptor.SocketAcceptorListener() {
+                public void event(Connection connection) {
+                    handleConnection(connection);
+                }
+            });
+            acceptorThread = new Thread(socketAcceptor);
+        } catch (UnknownHostException ex) {
+            System.err.println("Unknown host:");
+            ex.printStackTrace(System.err);
+        } catch (IOException ex) {
+            System.err.println("Failed to open socket:");
+            ex.printStackTrace(System.err);
+        }
+    }
+    
+    private void start() {
+        acceptorThread.start();
+    }
+    
+    private void handleConnection(Connection connection) {
+        connection.addMessageHandler(messageHandler);
     }
     
 }
