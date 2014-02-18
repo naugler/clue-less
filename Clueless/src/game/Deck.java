deec1bf3f100686760021a696899704012ea1b6f
PJenniferN <pjnaiman@gmail.com>
2014-02-17 21:45:28 -0500

    Added the client, sever, common modules to the eclipse project and continued adding to the basic architecture

--- Clueless/src/game/Deck.java
+++ Clueless/src/game/Deck.java
@@ -13,23 +13,39 @@ public class Deck {
 		}
 	}
 	
-	public void shuffle() 
+	/**
+	 * Shuffle cards between pos1 and pos2 inclusive.  This is useful when needing to 
+	 * shuffle each of the types separately for choosing the actual crime 
+	 * stuff (one weapon, one room, one person) 
+	 * pos2 should be greater than pos1
+	 */
+	public void shuffle(int pos1, int pos2) 
 	{
+		
 		int i, j;
+		if (pos1 >= 0 && pos1 < NUM_CARDS && pos2 >= 0 && pos2 < NUM_CARDS)
+		{
+			for (i = pos2; i >= pos1; i--) {
+				j = (int) ((i - pos1) * Math.random()) + pos1; // in the deck
 
-		for (i = 0; i < NUM_CARDS; i++) {
-			j = (int) (NUM_CARDS * Math.random()); // in the deck
-
-			/*
-			 * --------------------------------- Swap these randomly picked
-			 * cards ---------------------------------
-			 */
-			Card tmp = cards[i];
-			cards[i] = cards[j];
-			cards[j] = tmp;
+				/*
+				 * --------------------------------- Swap these randomly picked
+				 * cards ---------------------------------
+				 */
+				Card tmp = cards[i];
+				cards[i] = cards[j];
+				cards[j] = tmp;
+			}
+			curCard = 0;
 		}
-		curCard = 0;
+		else
+			System.out.println("ERROR: Cannot shuffle cards that do not exist");
 
+	}
+	
+	public void sort()
+	{
+		cards = Card.values();
 	}
 	
 	public Card deal()
