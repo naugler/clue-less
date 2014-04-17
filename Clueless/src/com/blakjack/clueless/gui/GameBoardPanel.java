163cefcbe355be764feef6947c1121b1e8eeef3b
naugler <>
2014-04-17 18:18:10 -0400

    basic player position painting

--- Clueless/src/com/blakjack/clueless/gui/GameBoardPanel.java
+++ Clueless/src/com/blakjack/clueless/gui/GameBoardPanel.java
@@ -1,8 +1,10 @@
 package com.blakjack.clueless.gui;
 
 import com.blakjack.clueless.common.Player;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import javax.swing.ImageIcon;
@@ -11,12 +13,21 @@ import javax.swing.JPanel;
 
 public class GameBoardPanel extends JPanel {
     
+    private final double PIECE_TILE_RATIO = 0.25; //size of the piece as % of tile size
+    
     private final ImageIcon gameboard;
     private List<Player> currentPlayers;
     
     public GameBoardPanel() {
         gameboard = new ImageIcon(getClass().getClassLoader().getResource("gameboard.png"));
         setPreferredSize(new Dimension(gameboard.getIconWidth(), gameboard.getIconHeight()));
+        
+        //just some in-place testing...
+        final Player mtard = new Player(Color.blue);
+        mtard.setPosition(6);
+        setPlayerPositions(new ArrayList<Player>() {{
+            add(mtard);
+        }});
     }
 
     // Gameboard image never changes we just need the pixel offsets
@@ -32,10 +43,22 @@ public class GameBoardPanel extends JPanel {
         arg0.drawImage(gameboard.getImage(), 0, 0, null);
         
         if (currentPlayers != null) {
+            int boardWidth = gameboard.getIconWidth();
+            int boardHeight = gameboard.getIconHeight();
+            int tileWidth = boardWidth / 5;
+            int tileHeight = boardHeight / 5;
             for (Player player : currentPlayers) {
-                player.getPosition();
+                int pos = player.getPosition();
+                int tileX = pos%5;
+                int tileY = pos/5;
                 
                 //draw the piece!
+                int width = (int)(tileWidth*PIECE_TILE_RATIO);
+                int height = (int)(tileHeight*PIECE_TILE_RATIO);
+                int x = tileX*tileWidth+(tileWidth-width)/2;
+                int y = tileY*tileHeight+(tileHeight-height)/2;
+                arg0.setColor(player.getCharacter().getColor());
+                arg0.fillOval(x, y, width, height);
             }
         }
     }
